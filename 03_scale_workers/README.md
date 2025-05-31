2dfacc7049e3fb8ab4d2ec8d88f5cf8aaf850710
Oskar Taubert <oskar.taubert@csc.fi>
2025-05-31 08:26:22 +0300

    worker scaling example

---
+++ 03_scale_workers/README.md
@@ -0,0 +1,6 @@
+# Scaling up
+- [ ] The RNG of each rank should be initialized differently.
+- [ ] increase the population size.
+- [ ] Instead of a `Propulator`, try `Islands`.
+- [ ] Compare pollination vs. migration.
+- [ ] Allocate more memory.
---
+++ 03_scale_workers/search.py
@@ -0,0 +1,34 @@
+import random
+from mpi4py import MPI
+import propulate
+
+def loss_fn(params):
+    return params["x"] ** 2 + params["y"] ** 2
+
+limits = {"x": (-5.12, 5.12), "y": (-5.12, 5.12)}
+generations = 100
+seed = 42
+rng=random.Random(seed + MPI.COMM_WORLD.rank)
+checkpoint_path="/tmp/pcheckpoints"
+propulate.utils.set_logger_config()
+pollination=False
+
+propagator = propulate.utils.get_default_propagator(
+        pop_size=200,
+        limits=limits,
+        rng=rng,
+        )
+
+islands = propulate.Islands(
+    loss_fn=loss_fn,
+    propagator=propagator,
+    rng=rng,
+    generations=100,
+    num_islands=2,
+    migration_probability=0.1,
+    pollination=pollination,
+    checkpoint_path=checkpoint_path,
+)
+
+islands.propulate()
+# islands.summarize()
